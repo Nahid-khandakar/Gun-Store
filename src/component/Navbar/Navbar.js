bbf9f84ce06836e8e883836652f6a8e1a1dac72c
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-03-25 12:42:54 +0600

    css customization

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Navbar.css'
+import { BsCartDashFill } from 'react-icons/bs';
 
 const Navbar = () => {
     return (
         <div className='navbar'>
             <h1>Samsu Store</h1>
+
+            <div className='cart-counter'>
+
+                <BsCartDashFill className='icon' color='#fff' />
+            </div>
         </div>
     );
 };
