4af867718c6fbfb2af07e717ac957b2712535a40
Nahid-khandakar <nahidkhandakar.11010@gmail.com>
2022-03-25 12:22:57 +0600

    icon and component fixed

--- src/component/Card/Card.js
+++ src/component/Card/Card.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import './Card.css'
+import { BsCartDashFill } from 'react-icons/bs';
 
 const Card = (props) => {
     const { name, img, bullet, capacity, price, action } = props.gun
     //console.log(props.gun)
     return (
         <div className='card'>
-            <img className='image-container' src={img} alt="" />
-            <h1>{name}</h1>
-            <p>Bullet Type : {bullet}</p>
-            <p>Capacity : {capacity}</p>
+
+            <div>
+                <img className='image-container' src={img} alt="" />
+            </div>
+
+            <div className='gun-info'>
+                <h1>{name}</h1>
+                <p>Bullet Type : {bullet}</p>
+                <p>Capacity : {capacity}</p>
+                <p>Action : {action}</p>
+            </div>
+
+            <div className='add-to-cart'>
+                <button onClick={handelAddToCart} > <BsCartDashFill className='icon' /> </button>
+                <h1>$ {price}</h1>
+            </div>
 
 
         </div>
